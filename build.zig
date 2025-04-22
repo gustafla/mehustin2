d8aefe05f0b2817867c1a60c4080aa1d77b35787
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-04-22 09:46:53 +0300

    build.zig: Add options compile-shaders and use-shaderc

--- build.zig
+++ build.zig
@@ -10,6 +10,16 @@ pub fn build(b: *std.Build) void {
     // Define variable for release setting
     const release_build = optimize != .Debug;
 
+    // Define build options
+    const options = b.addOptions();
+    const compile_shaders = b.option(bool, "compile-shaders", "Compile shaders at build time (requires shaderc)") orelse false;
+    const use_shaderc = b.option(bool, "use-shaderc", "Compile shaders at runtime (requires shaderc)") orelse !release_build;
+    options.addOption(bool, "use_shaderc", use_shaderc);
+
+    if (!compile_shaders and !use_shaderc) {
+        std.log.warn("Not using runtime shaderc but also compile_shaders is disabled. Shaders won't be built.", .{});
+    }
+
     // Add SDL3 dependency
     const sdl_dep = b.dependency("sdl", .{
         .target = target,
@@ -30,35 +40,45 @@ pub fn build(b: *std.Build) void {
         .strip = release_build,
     });
 
+    // Export build options defined earlier into the executable module
+    exe_mod.addOptions("options", options);
+
     // Link SDL
     exe_mod.linkLibrary(sdl_lib);
+
+    // Link shaderc
+    if (use_shaderc) {
+        exe_mod.linkSystemLibrary("shaderc", .{});
+    }
 
     // Build the main.zig exe
     const exe = b.addExecutable(.{
         .name = "mehustin2",
         .root_module = exe_mod,
     });
+    exe.linkLibC();
     // exe.lto = if (release_build) .full else null;
     exe.want_lto = release_build;
-    exe.linkLibC();
-
-    // Add shader compilation to the build graph
-    const compile_shaders_mod = b.createModule(.{
-        .root_source_file = b.path("./compile_shaders.zig"),
-        .target = b.resolveTargetQuery(.{}), // Native
-        .optimize = .Debug,
-    });
-    compile_shaders_mod.linkSystemLibrary("shaderc", .{});
-    const compile_shaders_exe = b.addExecutable(.{
-        .name = "compile_shaders",
-        .root_module = compile_shaders_mod,
-    });
-    compile_shaders_exe.linkLibC();
-    const compile_shaders_run = b.addRunArtifact(compile_shaders_exe);
-    b.getInstallStep().dependOn(&compile_shaders_run.step);
 
     // Configure the executable to be installed
     b.installArtifact(exe);
+
+    // Add shader compilation to the build graph if requested
+    if (compile_shaders) {
+        const compile_shaders_mod = b.createModule(.{
+            .root_source_file = b.path("./compile_shaders.zig"),
+            .target = b.resolveTargetQuery(.{}), // Native
+            .optimize = .Debug,
+        });
+        compile_shaders_mod.linkSystemLibrary("shaderc", .{});
+        const compile_shaders_exe = b.addExecutable(.{
+            .name = "compile_shaders",
+            .root_module = compile_shaders_mod,
+        });
+        compile_shaders_exe.linkLibC();
+        const compile_shaders_run = b.addRunArtifact(compile_shaders_exe);
+        b.getInstallStep().dependOn(&compile_shaders_run.step);
+    }
 
     // Create a Run step in the build graph
     const run_cmd = b.addRunArtifact(exe);
