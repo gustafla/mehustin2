e8c52f9225df1e5a3c429e6adb0e05c8432f65aa
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-04-23 16:14:39 +0300

    shader.zig: evaluate build option `use_shaderc`

--- src/shader.zig
+++ src/shader.zig
@@ -1,20 +1,33 @@
 const std = @import("std");
+const options = @import("options");
 const Allocator = std.mem.Allocator;
 const root = @import("root");
 const sdlerr = root.sdlerr;
 const config = root.config;
 const c = root.c;
 
-const Stage = enum(c_uint) {
+pub const Stage = enum(c_uint) {
     vert = c.SDL_GPU_SHADERSTAGE_VERTEX,
     frag = c.SDL_GPU_SHADERSTAGE_FRAGMENT,
 };
 
-pub fn loadShader(alloc: Allocator, device: *c.SDL_GPUDevice, name: []const u8) !*c.SDL_GPUShader {
-    // Determine shader stage from extension
+pub fn stageFromExtension(name: []const u8) !Stage {
     const extension = std.fs.path.extension(name);
     if (extension.len == 0) return error.NoStageExtension;
-    const stage = std.meta.stringToEnum(Stage, extension[1..]) orelse return error.NoStageExtension;
+    return std.meta.stringToEnum(Stage, extension[1..]) orelse error.NoStageExtension;
+}
+
+pub fn loadShader(alloc: Allocator, device: *c.SDL_GPUDevice, name: []const u8) !*c.SDL_GPUShader {
+    if (options.use_shaderc) {
+        return loadShaderGlsl(alloc, device, name);
+    } else {
+        return loadShaderSpirv(alloc, device, name);
+    }
+}
+
+fn loadShaderSpirv(alloc: Allocator, device: *c.SDL_GPUDevice, name: []const u8) !*c.SDL_GPUShader {
+    // Determine shader stage from extension
+    const stage = try stageFromExtension(name);
 
     // Allocate relative path to SPIR-V file
     const spirv_name = try std.mem.concat(alloc, u8, &[_][]const u8{ name, ".spv" });
@@ -25,7 +38,40 @@ pub fn loadShader(alloc: Allocator, device: *c.SDL_GPUDevice, name: []const u8) 
     // Load SPIR-V binary
     root.res_log.info("Loading {s}", .{path});
     const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
     const data = try file.readToEndAlloc(alloc, 1024 * 1024);
+    defer alloc.free(data);
+
+    // Load into SDL GPU
+    return try sdlerr(c.SDL_CreateGPUShader(device, &.{
+        .code_size = data.len,
+        .code = data.ptr,
+        .entrypoint = "main",
+        .format = c.SDL_GPU_SHADERFORMAT_SPIRV,
+        .stage = @intFromEnum(stage),
+        .num_samplers = 0,
+        .num_storage_textures = 0,
+        .num_storage_buffers = 0,
+        .num_uniform_buffers = 0,
+        .props = 0,
+    }));
+}
+
+fn loadShaderGlsl(alloc: Allocator, device: *c.SDL_GPUDevice, name: []const u8) !*c.SDL_GPUShader {
+    const shaderc = @import("shaderc.zig");
+
+    // Determine shader stage from extension
+    const stage = try stageFromExtension(name);
+
+    // Allocate full file path
+    const path = try std.fs.path.joinZ(alloc, &[_][]const u8{ config.shader_dir, name });
+    defer alloc.free(path);
+
+    // Read file and compile to SPIR-V
+    root.res_log.info("Loading {s}", .{path});
+    const glsl = try shaderc.loadFileNullTerminated(alloc, path);
+    defer alloc.free(glsl);
+    const data = try shaderc.compileShader(alloc, glsl, path);
     defer alloc.free(data);
 
     // Load into SDL GPU
