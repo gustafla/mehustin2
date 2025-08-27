88210ca702d1deaaf5656c19ab5ebd74c415405b
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-08-27 16:18:19 +0300

    More WIP work on render

--- src/render.zig
+++ src/render.zig
@@ -14,23 +14,39 @@ const Format = enum(c.SDL_GPUTextureFormat) {
     R16g16b16a16Float = c.SDL_GPU_TEXTUREFORMAT_R16G16B16A16_FLOAT,
 };
 
-const Texture = union(enum) {
-    framebuffer: usize,
+const Sampler = union(enum) {
+    fb: usize,
+};
+
+const RenderTarget = union(enum) {
+    fb: usize,
+    swapchain,
 };
 
 const Shader = struct {
     name: []const u8,
-    textures: []const Texture,
+    samplers: []const Sampler,
+};
+
+const Pipeline = struct {
+    vert: Shader,
+    frag: Shader,
+    targets: []const RenderTarget,
+};
+
+const DrawMesh = struct {
+    mesh: usize,
+    pipeline: usize,
 };
 
 const Draw = union(enum) {
     shader: usize,
-    mesh: usize,
+    mesh: DrawMesh,
 };
 
 const Pass = struct {
     draw: []const Draw,
-    targets: []const usize,
+    targets: []const RenderTarget,
 };
 
 const StaticMesh = struct {
@@ -44,7 +60,7 @@ const Mesh = union(enum) {
 
 const Scene = struct {
     framebuffers: []const Format,
-    shaders: []const Shader,
+    pipelines: []const Pipeline,
     meshes: []const Mesh,
     passes: []const Pass,
 };
@@ -56,9 +72,9 @@ pub const render_height: f32 = @floatFromInt(config.height);
 pub const render_aspect = render_width / render_height;
 
 var device: *c.SDL_GPUDevice = undefined;
-var pipelines: [scene.shaders.len]*c.SDL_GPUGraphicsPipeline = undefined;
-var pipeline3d: *c.SDL_GPUGraphicsPipeline = undefined;
+var pipelines: [scene.pipelines.len]*c.SDL_GPUGraphicsPipeline = undefined;
 var vertex_buffers: [scene.meshes.len]*c.SDL_GPUBuffer = undefined;
+var vertex_counts: [scene.meshes.len]u32 = undefined;
 
 pub fn deinit() void {
     for (vertex_buffers) |buf| {
@@ -67,8 +83,122 @@ pub fn deinit() void {
     for (pipelines) |pipeline| {
         c.SDL_ReleaseGPUGraphicsPipeline(device, pipeline);
     }
-    c.SDL_ReleaseGPUGraphicsPipeline(device, pipeline3d);
     c.SDL_DestroyGPUDevice(device);
+}
+
+fn initPipeline(alloc: Allocator, pipeline: Pipeline) !*c.SDL_GPUGraphicsPipeline {
+    const vert = try shader.loadShader(alloc, device, pipeline.vert.name, .{
+        .num_samplers = @as(u32, @intCast(pipeline.vert.samplers.len)),
+    });
+    defer c.SDL_ReleaseGPUShader(device, vert);
+    const frag = try shader.loadShader(alloc, device, pipeline.frag.name, .{
+        .num_samplers = @as(u32, @intCast(pipeline.frag.samplers.len)),
+    });
+    defer c.SDL_ReleaseGPUShader(device, frag);
+
+    const color_targets = try alloc.alloc(
+        c.SDL_GPUColorTargetDescription,
+        pipeline.targets.len,
+    );
+    defer alloc.free(color_targets);
+    for (pipeline.targets, color_targets) |target_def, *target| {
+        const format = switch (target_def) {
+            .fb => |fbi| @intFromEnum(scene.framebuffers[fbi]),
+            .swapchain => c.SDL_GetGPUSwapchainTextureFormat(
+                device,
+                root.window,
+            ),
+        };
+        target.* = .{ .format = format };
+    }
+
+    return try sdlerr(c.SDL_CreateGPUGraphicsPipeline(
+        device,
+        &std.mem.zeroInit(c.SDL_GPUGraphicsPipelineCreateInfo, .{
+            .vertex_shader = vert,
+            .fragment_shader = frag,
+            .primitive_type = c.SDL_GPU_PRIMITIVETYPE_TRIANGLESTRIP, // TODO: config
+            .rasterizer_state = .{
+                .fill_mode = c.SDL_GPU_FILLMODE_FILL,
+                .cull_mode = c.SDL_GPU_CULLMODE_NONE,
+                .front_face = c.SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE,
+            },
+            .multisample_state = .{
+                .sample_count = c.SDL_GPU_SAMPLECOUNT_1,
+            },
+            .target_info = .{
+                .num_color_targets = @as(u32, @intCast(color_targets.len)),
+                .color_target_descriptions = color_targets.ptr,
+            },
+        }),
+    ));
+}
+
+fn initVertexBufferStatic(mesh: StaticMesh) !*c.SDL_GPUBuffer {
+    var size: u32 = @intCast(mesh.coords.len * @sizeOf(f32));
+    if (mesh.colors) |colors| {
+        size += @intCast(colors.len * @sizeOf(f32));
+    }
+
+    const buffer = try sdlerr(c.SDL_CreateGPUBuffer(
+        device,
+        &.{
+            .size = size,
+            .usage = c.SDL_GPU_BUFFERUSAGE_VERTEX,
+            .props = 0,
+        },
+    ));
+
+    const transferbuf = try sdlerr(c.SDL_CreateGPUTransferBuffer(
+        device,
+        &.{
+            .size = size,
+            .usage = c.SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
+            .props = 0,
+        },
+    ));
+    defer c.SDL_ReleaseGPUTransferBuffer(device, transferbuf);
+    const data: [*]f32 = @ptrCast(@alignCast(try sdlerr(c.SDL_MapGPUTransferBuffer(
+        device,
+        transferbuf,
+        false,
+    ))));
+    @memcpy(data, mesh.coords);
+    if (mesh.colors) |colors| {
+        @memcpy(data[mesh.coords.len..], colors);
+    }
+    c.SDL_UnmapGPUTransferBuffer(device, transferbuf);
+
+    const cmdbuf = c.SDL_AcquireGPUCommandBuffer(device);
+    { // TODO: Remove this workaround when SDL is updated from 3.2.20
+        var swapchain_texture: ?*c.SDL_GPUTexture = undefined;
+        var width: u32 = undefined;
+        var height: u32 = undefined;
+        _ = c.SDL_AcquireGPUSwapchainTexture(
+            cmdbuf,
+            root.window,
+            &swapchain_texture,
+            &width,
+            &height,
+        );
+    }
+    const copy_pass = c.SDL_BeginGPUCopyPass(cmdbuf);
+    c.SDL_UploadToGPUBuffer(
+        copy_pass,
+        &.{
+            .offset = 0,
+            .transfer_buffer = transferbuf,
+        },
+        &.{
+            .size = size,
+            .offset = 0,
+            .buffer = buffer,
+        },
+        false,
+    );
+    c.SDL_EndGPUCopyPass(copy_pass);
+    try sdlerr(c.SDL_SubmitGPUCommandBuffer(cmdbuf));
+    return buffer;
 }
 
 pub fn init(alloc: Allocator) !void {
@@ -80,157 +210,76 @@ pub fn init(alloc: Allocator) !void {
     errdefer c.SDL_DestroyGPUDevice(device);
     try sdlerr(c.SDL_ClaimWindowForGPUDevice(device, root.window));
 
-    const vert = try shader.loadShader(alloc, device, "quad.vert", .{});
-    defer c.SDL_ReleaseGPUShader(device, vert);
-    const frag = try shader.loadShader(alloc, device, "shader.frag", .{});
-    defer c.SDL_ReleaseGPUShader(device, frag);
-
-    const swapchain_format = c.SDL_GetGPUSwapchainTextureFormat(
-        device,
-        root.window,
-    );
-    pipeline = try sdlerr(c.SDL_CreateGPUGraphicsPipeline(
-        device,
-        &std.mem.zeroInit(c.SDL_GPUGraphicsPipelineCreateInfo, .{
-            .vertex_shader = vert,
-            .fragment_shader = frag,
-            .primitive_type = c.SDL_GPU_PRIMITIVETYPE_TRIANGLESTRIP,
-            .rasterizer_state = .{
-                .fill_mode = c.SDL_GPU_FILLMODE_FILL,
-                .cull_mode = c.SDL_GPU_CULLMODE_NONE,
-                .front_face = c.SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE,
-            },
-            .multisample_state = .{
-                .sample_count = c.SDL_GPU_SAMPLECOUNT_1,
-            },
-            .target_info = .{
-                .num_color_targets = 1,
-                .color_target_descriptions = &[_]c.SDL_GPUColorTargetDescription{
-                    .{ .format = swapchain_format },
-                },
-            },
-        }),
-    ));
-    errdefer c.SDL_ReleaseGPUGraphicsPipeline(device, pipeline);
-
-    const vert3d = try shader.loadShader(
-        alloc,
-        device,
-        "3d.vert",
-        .{ .num_uniform_buffers = 1 },
-    );
-    defer c.SDL_ReleaseGPUShader(device, vert3d);
-    const frag3d = try shader.loadShader(alloc, device, "3d.frag", .{});
-    defer c.SDL_ReleaseGPUShader(device, frag3d);
-
-    pipeline3d = try sdlerr(c.SDL_CreateGPUGraphicsPipeline(
-        device,
-        &std.mem.zeroInit(c.SDL_GPUGraphicsPipelineCreateInfo, .{
-            .vertex_shader = vert3d,
-            .fragment_shader = frag3d,
-            .vertex_input_state = .{
-                .vertex_buffer_descriptions = &[_]c.SDL_GPUVertexBufferDescription{
-                    .{
-                        .slot = 0,
-                        .pitch = @sizeOf(f32) * 3,
-                        .input_rate = c.SDL_GPU_VERTEXINPUTRATE_VERTEX,
-                        .instance_step_rate = 0,
-                    },
-                },
-                .num_vertex_buffers = 1,
-                .vertex_attributes = &[_]c.SDL_GPUVertexAttribute{
-                    .{
-                        // a_Position
-                        .location = 0,
-                        .buffer_slot = 0,
-                        .format = c.SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
-                        .offset = 0,
-                    },
-                    .{
-                        // a_Color
-                        .location = 1,
-                        .buffer_slot = 0,
-                        .format = c.SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
-                        .offset = @sizeOf(@TypeOf(shape)),
-                    },
-                },
-                .num_vertex_attributes = 2,
-            },
-            .primitive_type = c.SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
-            .rasterizer_state = .{
-                .fill_mode = c.SDL_GPU_FILLMODE_FILL,
-                .cull_mode = c.SDL_GPU_CULLMODE_BACK,
-                .front_face = c.SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE,
-            },
-            .multisample_state = .{
-                .sample_count = c.SDL_GPU_SAMPLECOUNT_1,
-            },
-            .target_info = .{
-                .num_color_targets = 1,
-                .color_target_descriptions = &[_]c.SDL_GPUColorTargetDescription{
-                    .{ .format = swapchain_format },
-                },
-            },
-        }),
-    ));
-    errdefer c.SDL_ReleaseGPUGraphicsPipeline(device, pipeline3d);
-
-    vertex_buffer = try sdlerr(c.SDL_CreateGPUBuffer(
-        device,
-        &.{
-            .size = @sizeOf(@TypeOf(shape)) * 2,
-            .usage = c.SDL_GPU_BUFFERUSAGE_VERTEX,
-            .props = 0,
-        },
-    ));
-
-    const transferbuf = try sdlerr(c.SDL_CreateGPUTransferBuffer(
-        device,
-        &.{
-            .size = @sizeOf(@TypeOf(shape)) * 2,
-            .usage = c.SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
-            .props = 0,
-        },
-    ));
-    defer c.SDL_ReleaseGPUTransferBuffer(device, transferbuf);
-    const data: [*]f32 = @ptrCast(@alignCast(try sdlerr(c.SDL_MapGPUTransferBuffer(
-        device,
-        transferbuf,
-        false,
-    ))));
-    @memcpy(data, &shape);
-    @memcpy(data[shape.len..], &color);
-    c.SDL_UnmapGPUTransferBuffer(device, transferbuf);
-
-    const cmdbuf = c.SDL_AcquireGPUCommandBuffer(device);
-    { // TODO: Remove this workaround when SDL is updated from 3.2.20
-        var swapchain_texture: ?*c.SDL_GPUTexture = undefined;
-        var width: u32 = undefined;
-        var height: u32 = undefined;
-        try sdlerr(c.SDL_AcquireGPUSwapchainTexture(
-            cmdbuf,
-            root.window,
-            &swapchain_texture,
-            &width,
-            &height,
-        ));
+    for (scene.pipelines, &pipelines) |def, *pipeline| {
+        pipeline.* = try initPipeline(alloc, def);
     }
-    const copy_pass = c.SDL_BeginGPUCopyPass(cmdbuf);
-    c.SDL_UploadToGPUBuffer(
-        copy_pass,
-        &.{
-            .offset = 0,
-            .transfer_buffer = transferbuf,
-        },
-        &.{
-            .size = @sizeOf(@TypeOf(shape)) * 2,
-            .offset = 0,
-            .buffer = vertex_buffer,
-        },
-        false,
-    );
-    c.SDL_EndGPUCopyPass(copy_pass);
-    try sdlerr(c.SDL_SubmitGPUCommandBuffer(cmdbuf));
+
+    // const vert3d = try shader.loadShader(
+    //     alloc,
+    //     device,
+    //     "3d.vert",
+    //     .{ .num_uniform_buffers = 1 },
+    // );
+    // defer c.SDL_ReleaseGPUShader(device, vert3d);
+    // const frag3d = try shader.loadShader(alloc, device, "3d.frag", .{});
+    // defer c.SDL_ReleaseGPUShader(device, frag3d);
+
+    // pipeline3d = try sdlerr(c.SDL_CreateGPUGraphicsPipeline(
+    //     device,
+    //     &std.mem.zeroInit(c.SDL_GPUGraphicsPipelineCreateInfo, .{
+    //         .vertex_shader = vert3d,
+    //         .fragment_shader = frag3d,
+    //         .vertex_input_state = .{
+    //             .vertex_buffer_descriptions = &[_]c.SDL_GPUVertexBufferDescription{
+    //                 .{
+    //                     .slot = 0,
+    //                     .pitch = @sizeOf(f32) * 3,
+    //                     .input_rate = c.SDL_GPU_VERTEXINPUTRATE_VERTEX,
+    //                     .instance_step_rate = 0,
+    //                 },
+    //             },
+    //             .num_vertex_buffers = 1,
+    //             .vertex_attributes = &[_]c.SDL_GPUVertexAttribute{
+    //                 .{
+    //                     // a_Position
+    //                     .location = 0,
+    //                     .buffer_slot = 0,
+    //                     .format = c.SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
+    //                     .offset = 0,
+    //                 },
+    //                 .{
+    //                     // a_Color
+    //                     .location = 1,
+    //                     .buffer_slot = 0,
+    //                     .format = c.SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
+    //                     .offset = @sizeOf(@TypeOf(shape)),
+    //                 },
+    //             },
+    //             .num_vertex_attributes = 2,
+    //         },
+    //         .primitive_type = c.SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
+    //         .rasterizer_state = .{
+    //             .fill_mode = c.SDL_GPU_FILLMODE_FILL,
+    //             .cull_mode = c.SDL_GPU_CULLMODE_BACK,
+    //             .front_face = c.SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE,
+    //         },
+    //         .multisample_state = .{
+    //             .sample_count = c.SDL_GPU_SAMPLECOUNT_1,
+    //         },
+    //         .target_info = .{
+    //             .num_color_targets = 1,
+    //             .color_target_descriptions = &[_]c.SDL_GPUColorTargetDescription{
+    //                 .{ .format = swapchain_format },
+    //             },
+    //         },
+    //     }),
+    // ));
+    // errdefer c.SDL_ReleaseGPUGraphicsPipeline(device, pipeline3d);
+
+    for (scene.meshes, &vertex_buffers, &vertex_counts) |def, *buffer, *count| {
+        buffer.* = try initVertexBufferStatic(def.static);
+        count.* = @intCast(def.static.coords.len / 3);
+    }
 }
 
 pub fn render() !void {
@@ -254,25 +303,6 @@ pub fn render() !void {
         };
     };
 
-    const color_target_info = std.mem.zeroInit(c.SDL_GPUColorTargetInfo, .{
-        .texture = swapchain_texture,
-        .clear_color = .{ 0.0, 0.0, 0.0, 1.0 },
-        .load_op = c.SDL_GPU_LOADOP_CLEAR,
-        .store_op = c.SDL_GPU_STOREOP_STORE,
-    });
-    const render_pass = c.SDL_BeginGPURenderPass(
-        cmdbuf,
-        &color_target_info,
-        1,
-        null,
-    );
-    c.SDL_SetGPUViewport(render_pass, &viewport(width, height));
-
-    // Background
-    c.SDL_BindGPUGraphicsPipeline(render_pass, pipeline);
-    c.SDL_DrawGPUPrimitives(render_pass, 4, 1, 0, 0);
-
-    // 3D scene
     const t = time.getTime();
     const matrices = extern struct {
         projection: math.Mat4,
@@ -294,20 +324,50 @@ pub fn render() !void {
             math.vec3.YUP,
         ),
     };
-    c.SDL_PushGPUVertexUniformData(
-        cmdbuf,
-        0,
-        @ptrCast(&matrices),
-        @sizeOf(@TypeOf(matrices)),
-    );
-    c.SDL_BindGPUGraphicsPipeline(render_pass, pipeline3d);
-    c.SDL_BindGPUVertexBuffers(render_pass, 0, &[_]c.SDL_GPUBufferBinding{.{
-        .buffer = vertex_buffer,
-        .offset = 0,
-    }}, 1);
-    c.SDL_DrawGPUPrimitives(render_pass, shape.len / 3, 1, 0, 0);
 
-    c.SDL_EndGPURenderPass(render_pass);
+    for (scene.passes) |pass| {
+        const color_target_infos = std.mem.zeroInit(c.SDL_GPUColorTargetInfo, .{
+            .texture = swapchain_texture, // TODO: framebuffers
+            .clear_color = .{ 0.0, 0.0, 0.0, 1.0 },
+            .load_op = c.SDL_GPU_LOADOP_CLEAR,
+            .store_op = c.SDL_GPU_STOREOP_STORE,
+        });
+        const render_pass = c.SDL_BeginGPURenderPass(
+            cmdbuf,
+            &color_target_infos,
+            1,
+            null,
+        );
+        c.SDL_SetGPUViewport(render_pass, &viewport(width, height));
+
+        for (pass.draw) |draw| {
+            switch (draw) {
+                .shader => |i| {
+                    c.SDL_BindGPUGraphicsPipeline(render_pass, pipelines[i]);
+                    c.SDL_DrawGPUPrimitives(render_pass, 4, 1, 0, 0);
+                },
+                .mesh => |draw_mesh| {
+                    // 3D scene
+                    c.SDL_PushGPUVertexUniformData(
+                        cmdbuf,
+                        0,
+                        @ptrCast(&matrices),
+                        @sizeOf(@TypeOf(matrices)),
+                    );
+                    const pipeline = pipelines[draw_mesh.pipeline];
+                    c.SDL_BindGPUGraphicsPipeline(render_pass, pipeline);
+                    c.SDL_BindGPUVertexBuffers(render_pass, 0, &[_]c.SDL_GPUBufferBinding{.{
+                        // TODO: document assumption that 1 mesh = 1 vertex buffer
+                        .buffer = vertex_buffers[draw_mesh.mesh],
+                        .offset = 0,
+                    }}, 1);
+                    c.SDL_DrawGPUPrimitives(render_pass, vertex_counts[draw_mesh.mesh], 1, 0, 0);
+                },
+            }
+        }
+
+        c.SDL_EndGPURenderPass(render_pass);
+    }
 
     try sdlerr(c.SDL_SubmitGPUCommandBuffer(cmdbuf));
 }
