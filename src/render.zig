8fc992236bf6b6815e3c5a21a53fcb35b72450b1
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2026-01-06 23:21:55 +0200

    render.zig: @cInclude only SDL_gpu.h, not SDL.h

--- src/render.zig
+++ src/render.zig
@@ -15,7 +15,7 @@ const shader = @import("shader.zig");
 
 pub const c = @cImport({
     @cDefine("SDL_DISABLE_OLD_NAMES", {});
-    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_gpu.h");
     @cInclude("stb_image.h");
     @cInclude("stb_truetype.h");
 });
