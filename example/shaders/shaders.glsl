8d65d30c6b91ba4d39d6b9b077ce41267112365f
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2026-04-27 10:54:03 +0300

    example: Add more cubes

--- example/shaders/shaders.glsl
+++ example/shaders/shaders.glsl
@@ -1,9 +1,6 @@
 #version 450
 
 #ifdef VERTEX
-layout(location = 0) in vec3 in_position;
-layout(location = 1) in vec3 in_normal;
-
 layout(std140, set = 1, binding = 0) uniform VertexFrameData {
     mat4 u_view_projection;
     vec4 u_cam_pos;
@@ -11,29 +8,56 @@ layout(std140, set = 1, binding = 0) uniform VertexFrameData {
     vec4 u_cam_up;
     float u_time;
 };
-
-layout(location = 0) out vec3 out_position;
-layout(location = 1) out vec3 out_normal;
-
-void main() {
-    vec3 cam_position = u_cam_pos.xyz;
-    out_position = in_position - cam_position;
-    out_normal = in_normal;
-    vec4 clip_position = u_view_projection * vec4(in_position, 1.);
-    gl_Position = clip_position;
-}
 #endif // VERTEX
 
 #ifdef FRAGMENT
+layout(std140, set = 3, binding = 0) uniform FragmentFrameData {
+    float u_time;
+};
+#endif // FRAGMENT
+
+#ifdef GRAPHICS_MAIN
+layout(location = 0) IO Interface {
+vec3 position;
+vec3 normal;
+vec3 emissive;
+} io;
+#endif // GRAPHICS_MAIN
+
+#ifdef VERTEX_MAIN
 layout(location = 0) in vec3 in_position;
 layout(location = 1) in vec3 in_normal;
 
+layout(location = 8) in vec4 in_inst_translation_scale;
+layout(location = 9) in vec4 in_inst_rotation;
+layout(location = 10) in vec4 in_inst_emissive;
+
+#include <transform.glsl>
+
+void main() {
+    vec3 cam_position = u_cam_pos.xyz;
+
+    const float scale = in_inst_translation_scale.w;
+    const vec3 translation = in_inst_translation_scale.xyz;
+    const vec4 rotation = in_inst_rotation;
+    vec3 rotated_position = rotateVector(in_position, rotation) * scale;
+    vec3 translated_position = rotated_position + translation;
+
+    io.position = translated_position - cam_position;
+    io.normal = rotateVector(in_normal, rotation);
+    io.emissive = in_inst_emissive.rgb * in_inst_emissive.a;
+
+    vec4 clip_position = u_view_projection * vec4(translated_position, 1.);
+    gl_Position = clip_position;
+}
+#endif // VERTEX_MAIN
+
+#ifdef FRAGMENT_MAIN
 layout(location = 0) out vec4 out_color;
 
 void main() {
-    vec3 dir = normalize(-in_position);
-    float lighting = max(dot(dir, in_normal), 0.0);
-    vec3 color = abs(in_position) * 0.5;
-    out_color = vec4(lighting * color, 1.0);
+    vec3 dir = normalize(-io.position);
+    float lighting = max(dot(dir, io.normal), 0.5);
+    out_color = vec4(lighting * io.emissive, 1.0);
 }
-#endif // FRAGMENT
+#endif // FRAGMENT_MAIN
--- example/src/script.zig
+++ example/src/script.zig
@@ -8,6 +8,7 @@ const TextureInfo = types.TextureInfo;
 const BufferInfo = types.BufferInfo;
 const timeline = engine.timeline;
 const util = engine.util;
+const math = engine.math;
 
 pub const config = struct {
     pub const main = @import("config.zon");
@@ -51,13 +52,19 @@ pub const texture = struct {
 pub const Texture = std.meta.DeclEnum(texture);
 
 pub const layout = struct {
-    pub const InstanceText = timeline.InstanceText;
-
     pub const VertexPosNormal = extern struct {
         position: [3]f32,
         normal: [3]f32,
 
         pub const locations = .{ 0, 1 };
+    };
+
+    pub const Instance = extern struct {
+        translation_scale: [4]f32,
+        rotation: [4]f32,
+        emissive: [4]f32,
+
+        pub const locations = .{ 8, 9, 10 };
     };
 };
 
@@ -100,6 +107,36 @@ pub const buffer = struct {
 
         pub fn init(dst: []Layout, _: *BufferInfo) !void {
             util.interleave(Layout, dst, .{ &position, &normal });
+        }
+    };
+
+    pub const cube_instances = struct {
+        pub const Layout = layout.Instance;
+
+        const nx = 19;
+        const ny = 11;
+        pub const num_elements = nx * ny;
+
+        pub fn updateData(dst: []Layout) !void {
+            for (0..num_elements) |i| {
+                const y: f32 = @floatFromInt(i / nx);
+                const x: f32 = @floatFromInt(i % nx);
+                const fi: f32 = @floatFromInt(i);
+                const color = util.hslToRgb(.{ fi / 4, 1, fi / num_elements });
+                dst[i] = .{
+                    .translation_scale = .{
+                        x - (nx / 2),
+                        y - (ny / 2),
+                        @cos(x + frame.state.time * 0.7) * 0.2 + @cos(y + frame.state.time) * 0.2,
+                        0.125,
+                    },
+                    .rotation = math.quat.mul(
+                        math.quat.fromAxisAngle(.{ 0, 1, 0 }, frame.state.time + x * 0.1),
+                        math.quat.fromAxisAngle(.{ 0, 0, 1 }, frame.state.time + y * 0.27),
+                    ),
+                    .emissive = .{ color[0], color[1], color[2], (fi / num_elements) * 5 },
+                };
+            }
         }
     };
 };
