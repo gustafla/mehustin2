1307fd74946064c3f76397b5b943464bb8d79036
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2026-07-31 11:45:38 +0300

    example: comment out gaussian kernel

--- example/shaders/data.zig
+++ example/shaders/data.zig
@@ -2,26 +2,22 @@ const std = @import("std");
 
 const getParam = @import("genglsl").getParam;
 
-pub const kernel = struct {
-    const size = getParam("blur_radius");
-    const sigma: comptime_float = getParam("blur_sigma");
-
-    pub fn gaussian() [size + 1]f32 {
-        var buffer: [size + 1]f32 = undefined;
-
-        for (0..buffer.len) |i| buffer[i] = g(@floatFromInt(i));
-        normalize(&buffer);
-        return buffer;
-    }
-
-    fn g(x: f32) f32 {
-        return (1.0 / (sigma * @sqrt(2.0 * std.math.pi))) *
-            (@exp((-1.0 / 2.0) * ((x * x) / (sigma * sigma))));
-    }
-};
-
-fn normalize(xs: []f32) void {
-    var sum: f32 = 0;
-    for (xs) |x| sum += x;
-    for (xs) |*x| x.* /= sum;
-}
+// // Example: this generates a generated.glsl-file containing `kernel_gaussian`
+// // as a static array.
+// pub const kernel = struct {
+//     const size = getParam("blur_radius");
+//     const sigma: comptime_float = getParam("blur_sigma");
+//
+//     pub fn gaussian() [size + 1]f32 {
+//         var buffer: [size + 1]f32 = undefined;
+//
+//         for (0..buffer.len) |i| buffer[i] = g(i);
+//         return buffer;
+//     }
+//
+//     fn g(x: anytype) f32 {
+//         const x_f32: f32 = if (@TypeOf(x) == f32) x else @floatFromInt(x);
+//         return (1.0 / (sigma * @sqrt(2.0 * std.math.pi))) *
+//             (@exp((-1.0 / 2.0) * ((x_f32 * x_f32) / (sigma * sigma))));
+//     }
+// };
